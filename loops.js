0b1e6abd58043a85bb43e38d0cc767096aaeb677
lanasgoddard <lanasgoddard@gmail.com>
2018-12-26 10:18:39 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,19 @@
+function forLoop(array){
+  for(var i = 0; i <25; i++){
+    if (i === 1){
+      array.push(`I am ${i} strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
+  }
+}
+return array
+}
+
+function whileLoop(n) {
+  let e = n;
+
+    while (e > 0) {
+      console.log(--e)
+    }
+    return "done";
+  }
